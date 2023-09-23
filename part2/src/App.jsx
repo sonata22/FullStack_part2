b0990b56565e31e82af948c9165a4cad9805b363
sonata22 <nataliia.sosnovshchenko@gmail.com>
2023-09-23 21:06:41 +0300

    Starting off with a new project and base for application from exercises 1.1-1.5

---
+++ part2/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    <Part
+      part={parts[0]} 
+    />
+    <Part
+      part={parts[1]} 
+    />
+    <Part
+      part={parts[2]} 
+    />      
+  </>
+
+const Course = () => {
+
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+    </div>
+  )
+}
+
+export default App
