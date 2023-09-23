67bffa3794b888e72e81f7cd5c212e80c3eeae03
sonata22 <nataliia.sosnovshchenko@gmail.com>
2023-09-23 21:26:15 +0300

    finished exercise 2.1

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,41 +1,43 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Part = ({ part }) => 
+const Part = ({ part }) =>
   <p>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
-
-const Course = () => {
-
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) =>
+        <Part key={part.id} part={part} />
+      )}
+    </>)
 }
 
-
+const Course = ({ course, parts }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content parts={parts} />
+    </>
+  )
+}
 
 const App = () => {
   const course = 'Half Stack application development'
   const parts = [
     {
+      id: 0,
       name: 'Fundamentals of React',
       exercises: 10
     },
     {
+      id: 1,
       name: 'Using props to pass data',
       exercises: 7
     },
     {
+      id: 2,
       name: 'State of a component',
       exercises: 14
     }
@@ -43,8 +45,7 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Course course={course} parts={parts} />
     </div>
   )
 }
